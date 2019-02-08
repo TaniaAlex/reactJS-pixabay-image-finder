12a0edb246ae4d055eff3180f94fb2088efdd23a
user <user@users-MBP.fritz.box>
2019-02-08 15:33:17 +0100

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+
+const NavBar = () => <AppBar title="Pixabay Image Finder" />;
+
+export default NavBar;
